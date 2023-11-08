p1gp1g/mollyim-android

Add unifiedpush settings

app/src/unifiedpush/java/im/molly/unifiedpush/components/settings/app/notifications/UnifiedPushSettingsFragment.kt
@@ -1,0 +1,193 @@
+package im.molly.unifiedpush.components.settings.app.notifications
+
+import android.content.DialogInterface
+import android.content.res.Resources
+import android.text.InputType
+import android.widget.EditText
+import android.widget.FrameLayout
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import im.molly.unifiedpush.model.FetchStrategy
+import im.molly.unifiedpush.model.toFetchStrategy
+import im.molly.unifiedpush.model.toInt
+import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.components.settings.DSLConfiguration
+import org.thoughtcrime.securesms.components.settings.DSLSettingsFragment
+import org.thoughtcrime.securesms.components.settings.DSLSettingsIcon
+import org.thoughtcrime.securesms.components.settings.DSLSettingsText
+import org.thoughtcrime.securesms.components.settings.configure
+import org.thoughtcrime.securesms.util.Util.writeTextToClipboard
+import org.thoughtcrime.securesms.util.adapter.mapping.MappingAdapter
+
+class UnifiedPushSettingsFragment : DSLSettingsFragment(R.string.NotificationsSettingsFragment__unifiedpush) {
+
+  private lateinit var viewModel: UnifiedPushSettingsViewModel
+
+  override fun bindAdapter(adapter: MappingAdapter) {
+    val factory = UnifiedPushSettingsViewModel.Factory(requireActivity().application)
+
+    viewModel = ViewModelProvider(this, factory)[UnifiedPushSettingsViewModel::class.java]
+
+    viewModel.state.observe(viewLifecycleOwner) {
+      adapter.submitList(getConfiguration(it).toMappingModelList())
+    }
+  }
+
+  private fun getConfiguration(state: UnifiedPushSettingsState): DSLConfiguration {
+    return configure {
+      textPref(
+        title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__status)),
+        summary = DSLSettingsText.from(getStatusSummary(state)),
+      )
+
+      switchPref(
+        title = DSLSettingsText.from(R.string.UnifiedPushSettingsFragment__enabled),
+        isChecked = state.enabled,
+        onClick = {
+          viewModel.setUnifiedPushEnabled(!state.enabled)
+        }
+      )
+
+      if (state.enabled) {
+
+        switchPref(
+          title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__air_gaped)),
+          isChecked = state.airGaped,
+          onClick = {
+            viewModel.setUnifiedPushAirGaped(!state.airGaped)
+          }
+        )
+
+        radioListPref(
+
+          title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__strategy)),
+          listItems = listOf(getString(R.string.UnifiedPushSettingsFragment__strategy_websocket), getString(R.string.UnifiedPushSettingsFragment__strategy_rest)).toTypedArray(),
+          selected = state.fetchStrategy.toInt(),
+          onSelected = {
+            viewModel.setFetchStrategy(it.toFetchStrategy())
+          },
+        )
+
+        val distributors = {
+          radioListPref(
+            title = DSLSettingsText.from(R.string.UnifiedPushSettingsFragment__method),
+            listItems = state.distributors.map { it.name }.toTypedArray(),
+            selected = state.selected,
+            onSelected = {
+              viewModel.setUnifiedPushDistributor(state.distributors[it].applicationId)
+            },
+          )
+        }
+
+        if (state.airGaped) {
+
+          distributors()
+
+          clickPref(
+            title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__server_parameters)),
+            summary = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__click_to_copy)),
+            iconEnd = DSLSettingsIcon.from(R.drawable.ic_copy_24),
+            onClick = { writeTextToClipboard(requireContext(), "Server parameters", getServerParameters(state)) },
+          )
+
+        } else {
+
+          clickPref(
+            title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__account_id)),
+            summary = DSLSettingsText.from(state.device?.uuid ?: getString(R.string.UnifiedPushSettingsFragment__unknown)),
+            iconEnd = DSLSettingsIcon.from(R.drawable.ic_copy_24),
+            onClick = {
+              writeTextToClipboard(requireContext(), "Account ID", state.device?.uuid ?: getString(R.string.UnifiedPushSettingsFragment__unknown))
+                      },
+          )
+
+          clickPref(
+            title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__server_url)),
+            summary = DSLSettingsText.from(state.mollySocketUrl ?: getString(R.string.UnifiedPushSettingsFragment__no_server_url_summary)),
+            iconEnd = getMollySocketUrlIcon(state),
+            onClick = { urlDialog(state) },
+          )
+
+          distributors()
+        }
+      }
+    }
+  }
+
+  private fun getServerParameters(state: UnifiedPushSettingsState): String {
+    val device = state.device ?: return getString(R.string.UnifiedPushSettingsFragment__no_device)
+    val endpoint = state.endpoint ?: return getString(R.string.UnifiedPushSettingsFragment__no_endpoint)
+    val strategy = when (state.fetchStrategy) {
+      FetchStrategy.WEBSOCKET -> "websocket"
+      FetchStrategy.REST -> "rest"
+    }
+    return "connection add ${device.uuid} ${device.deviceId} ${device.password} $endpoint $strategy"
+  }
+
+  private fun urlDialog(state: UnifiedPushSettingsState) {
+    val alertDialog = MaterialAlertDialogBuilder(requireContext())
+    val input = EditText(requireContext()).apply {
+      inputType = InputType.TYPE_TEXT_VARIATION_URI
+      setText(state.mollySocketUrl)
+    }
+    alertDialog.setEditText(
+      input
+    )
+    alertDialog.setPositiveButton(getString(R.string.UnifiedPushSettingsFragment__ok)) { _: DialogInterface, _: Int ->
+      viewModel.setMollySocketUrl(input.text.toString())
+    }
+    alertDialog.show()
+
+  }
+
+  private val Float.toPx: Int
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+  private fun MaterialAlertDialogBuilder.setEditText(editText: EditText): MaterialAlertDialogBuilder {
+    val container = FrameLayout(context)
+    container.addView(editText)
+    val containerParams = FrameLayout.LayoutParams(
+      FrameLayout.LayoutParams.MATCH_PARENT,
+      FrameLayout.LayoutParams.WRAP_CONTENT
+    )
+    val marginHorizontal = 48F
+    val marginTop = 16F
+    containerParams.topMargin = (marginTop / 2).toPx
+    containerParams.leftMargin = marginHorizontal.toInt()
+    containerParams.rightMargin = marginHorizontal.toInt()
+    container.layoutParams = containerParams
+
+    val superContainer = FrameLayout(context)
+    superContainer.addView(container)
+
+    setView(superContainer)
+
+    return this
+  }
+
+  private fun getStatusSummary(state: UnifiedPushSettingsState): String {
+    if (!state.enabled) {
+      return getString(R.string.UnifiedPushSettingsFragment__status_summary_disabled)
+    }
+    if (state.airGaped) {
+      return getString(R.string.UnifiedPushSettingsFragment__status_summary_air_gaped)
+    }
+    if (state.mollySocketUrl.isNullOrBlank()) {
+      return getString(R.string.UnifiedPushSettingsFragment__status_summary_mollysocket_url_missing)
+    }
+    return when (state.mollySocketOk) {
+      null -> getString(R.string.UnifiedPushSettingsFragment__status_summary_pending)
+      true -> getString(R.string.UnifiedPushSettingsFragment__ok)
+      false -> getString(R.string.UnifiedPushSettingsFragment__status_summary_mollysocket_server_not_found)
+    }
+  }
+
+  private fun getMollySocketUrlIcon(state: UnifiedPushSettingsState): DSLSettingsIcon? {
+    if (state.mollySocketUrl == null || state.mollySocketOk == null ) return null
+    return if (state.mollySocketOk) {
+      DSLSettingsIcon.from(R.drawable.ic_check_20)
+    } else {
+      DSLSettingsIcon.from(R.drawable.ic_alert)
+    }
+  }
+}

app/src/unifiedpush/java/im/molly/unifiedpush/components/settings/app/notifications/UnifiedPushSettingsViewModel.kt
@@ -1,0 +1,124 @@
+package im.molly.unifiedpush.components.settings.app.notifications
+
+import android.app.Application
+import android.content.pm.PackageManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import im.molly.unifiedpush.model.FetchStrategy
+import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.keyvalue.SignalStore
+import org.thoughtcrime.securesms.util.livedata.Store
+import org.unifiedpush.android.connector.UnifiedPush
+
+class UnifiedPushSettingsViewModel(private val application: Application) : ViewModel() {
+
+  private val store = Store(getState())
+
+  val state: LiveData<UnifiedPushSettingsState> = store.stateLiveData
+  var checkingServer = false
+
+  private fun getState(): UnifiedPushSettingsState {
+    val distributor = UnifiedPush.getDistributor(application)
+    var count = -1
+    var selected = -1
+
+    var distributors = UnifiedPush.getDistributors(application).map {
+      count++
+      if (it == distributor) {
+        selected = count
+      }
+      Distributor(
+        applicationId = it,
+        name = try {
+          val ai = application.packageManager.getApplicationInfo(it, 0)
+          /* When Android 13 will be supported:
+          val ai = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            application.packageManager.getApplicationInfo(it,
+              PackageManager.ApplicationInfoFlags.of(
+                PackageManager.GET_META_DATA.toLong()
+              )
+            )
+          } else {
+            application.packageManager.getApplicationInfo(it, 0)
+          }*/
+          application.packageManager.getApplicationLabel(ai)
+        } catch (e: PackageManager.NameNotFoundException) {
+          it
+        } as String)
+    }
+
+    if (distributors.isEmpty()) {
+      distributors = listOf(Distributor(
+        name = application.getString(R.string.UnifiedPushSettingsViewModel__no_distributor),
+        applicationId = "",
+      ))
+    }
+
+    return UnifiedPushSettingsState(
+      enabled = SignalStore.unifiedpush().enabled,
+      airGaped = SignalStore.unifiedpush().airGaped,
+      device = SignalStore.unifiedpush().device,
+      distributors = distributors,
+      selected = selected,
+      endpoint = SignalStore.unifiedpush().endpoint,
+      mollySocketUrl = SignalStore.unifiedpush().mollySocketUrl,
+      mollySocketOk = if (checkingServer) { null } else { SignalStore.unifiedpush().mollySocketOk },
+      fetchStrategy = SignalStore.unifiedpush().fetchStrategy
+    )
+  }
+
+  fun setUnifiedPushEnabled(enabled: Boolean) {
+    if (enabled) {
+      UnifiedPush.getDistributors(application).getOrNull(0)?.let {
+        UnifiedPush.saveDistributor(application, it)
+        UnifiedPush.registerApp(application)
+        // Do not enable if there is no distributor
+      } ?: return
+    } else {
+      UnifiedPush.unregisterApp(application)
+    }
+    SignalStore.unifiedpush().enabled = enabled
+    registerMollySocketServer()
+  }
+
+  fun setUnifiedPushAirGaped(airGaped: Boolean) {
+    SignalStore.unifiedpush().airGaped = airGaped
+    registerMollySocketServer()
+  }
+
+  fun setFetchStrategy(strategy: FetchStrategy) {
+    SignalStore.unifiedpush().fetchStrategy = strategy
+    registerMollySocketServer()
+  }
+
+  fun setUnifiedPushDistributor(distributor: String) {
+    UnifiedPush.saveDistributor(application, distributor)
+    UnifiedPush.registerApp(application)
+    store.update { getState() }
+  }
+
+  fun setMollySocketUrl(url: String?) {
+    SignalStore.unifiedpush().mollySocketUrl = url
+    registerMollySocketServer()
+  }
+
+  private fun registerMollySocketServer() {
+    if (with(SignalStore.unifiedpush()) {
+        this.enabled && !this.airGaped && this.endpoint.isNullOrBlank()
+      }) {
+      checkingServer = true
+      store.update { getState() }
+      //TODO
+      SignalStore.unifiedpush().mollySocketOk = true
+      checkingServer = false
+    }
+    store.update { getState() }
+  }
+
+  class Factory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+      return requireNotNull(modelClass.cast(UnifiedPushSettingsViewModel(application)))
+    }
+  }
+}

mollyim/mollyim-android-unifiedpush

app/src/unifiedpush/java/im/molly/unifiedpush/components/settings/app/notifications/UnifiedPushSettingsFragment.kt
@@ -1,0 +1,176 @@
+package im.molly.unifiedpush.components.settings.app.notifications
+
+import android.content.DialogInterface
+import android.content.res.Resources
+import android.text.InputType
+import android.widget.EditText
+import android.widget.FrameLayout
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.components.settings.DSLConfiguration
+import org.thoughtcrime.securesms.components.settings.DSLSettingsFragment
+import org.thoughtcrime.securesms.components.settings.DSLSettingsIcon
+import org.thoughtcrime.securesms.components.settings.DSLSettingsText
+import org.thoughtcrime.securesms.components.settings.configure
+import org.thoughtcrime.securesms.util.Util.writeTextToClipboard
+import org.thoughtcrime.securesms.util.adapter.mapping.MappingAdapter
+
+class UnifiedPushSettingsFragment : DSLSettingsFragment(R.string.NotificationsSettingsFragment__unifiedpush) {
+
+  private lateinit var viewModel: UnifiedPushSettingsViewModel
+
+  override fun bindAdapter(adapter: MappingAdapter) {
+    val factory = UnifiedPushSettingsViewModel.Factory(requireActivity().application)
+
+    viewModel = ViewModelProvider(this, factory)[UnifiedPushSettingsViewModel::class.java]
+
+    viewModel.state.observe(viewLifecycleOwner) {
+      adapter.submitList(getConfiguration(it).toMappingModelList())
+    }
+  }
+
+  private fun getConfiguration(state: UnifiedPushSettingsState): DSLConfiguration {
+    return configure {
+      textPref(
+        title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__status)),
+        summary = DSLSettingsText.from(getStatusSummary(state)),
+      )
+
+      switchPref(
+        title = DSLSettingsText.from(R.string.UnifiedPushSettingsFragment__enabled),
+        isChecked = state.enabled,
+        onClick = {
+          viewModel.setUnifiedPushEnabled(!state.enabled)
+        }
+      )
+
+      if (state.enabled) {
+
+        switchPref(
+          title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__air_gaped)),
+          isChecked = state.airGaped,
+          onClick = {
+            viewModel.setUnifiedPushAirGaped(!state.airGaped)
+          }
+        )
+
+        val distributors = {
+          radioListPref(
+            title = DSLSettingsText.from(R.string.UnifiedPushSettingsFragment__method),
+            listItems = state.distributors.map { it.name }.toTypedArray(),
+            selected = state.selected,
+            onSelected = {
+              viewModel.setUnifiedPushDistributor(state.distributors[it].applicationId)
+            },
+          )
+        }
+
+        if (state.airGaped) {
+
+          distributors()
+
+          clickPref(
+            title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__server_parameters)),
+            summary = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__click_to_copy)),
+            iconEnd = DSLSettingsIcon.from(R.drawable.symbol_copy_android_24),
+            onClick = { writeTextToClipboard(requireContext(), "Server parameters", getServerParameters(state)) },
+          )
+
+        } else {
+
+          clickPref(
+            title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__account_id)),
+            summary = DSLSettingsText.from(state.device?.uuid ?: getString(R.string.UnifiedPushSettingsFragment__unknown)),
+            iconEnd = DSLSettingsIcon.from(R.drawable.symbol_copy_android_24),
+            onClick = {
+              writeTextToClipboard(requireContext(), "Account ID", state.device?.uuid ?: getString(R.string.UnifiedPushSettingsFragment__unknown))
+                      },
+          )
+
+          clickPref(
+            title = DSLSettingsText.from(getString(R.string.UnifiedPushSettingsFragment__server_url)),
+            summary = DSLSettingsText.from(state.mollySocketUrl ?: getString(R.string.UnifiedPushSettingsFragment__no_server_url_summary)),
+            iconEnd = getMollySocketUrlIcon(state),
+            onClick = { urlDialog(state) },
+          )
+
+          distributors()
+        }
+      }
+    }
+  }
+
+  private fun getServerParameters(state: UnifiedPushSettingsState): String {
+    val device = state.device ?: return getString(R.string.UnifiedPushSettingsFragment__no_device)
+    val endpoint = state.endpoint ?: return getString(R.string.UnifiedPushSettingsFragment__no_endpoint)
+    return "connection add ${device.uuid} ${device.deviceId} ${device.password} $endpoint websocket"
+  }
+
+  private fun urlDialog(state: UnifiedPushSettingsState) {
+    val alertDialog = MaterialAlertDialogBuilder(requireContext())
+    val input = EditText(requireContext()).apply {
+      inputType = InputType.TYPE_TEXT_VARIATION_URI
+      setText(state.mollySocketUrl)
+    }
+    alertDialog.setEditText(
+      input
+    )
+    alertDialog.setPositiveButton(getString(R.string.UnifiedPushSettingsFragment__ok)) { _: DialogInterface, _: Int ->
+      viewModel.setMollySocketUrl(input.text.toString())
+    }
+    alertDialog.show()
+
+  }
+
+  private val Float.toPx: Int
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+  private fun MaterialAlertDialogBuilder.setEditText(editText: EditText): MaterialAlertDialogBuilder {
+    val container = FrameLayout(context)
+    container.addView(editText)
+    val containerParams = FrameLayout.LayoutParams(
+      FrameLayout.LayoutParams.MATCH_PARENT,
+      FrameLayout.LayoutParams.WRAP_CONTENT
+    )
+    val marginHorizontal = 48F
+    val marginTop = 16F
+    containerParams.topMargin = (marginTop / 2).toPx
+    containerParams.leftMargin = marginHorizontal.toInt()
+    containerParams.rightMargin = marginHorizontal.toInt()
+    container.layoutParams = containerParams
+
+    val superContainer = FrameLayout(context)
+    superContainer.addView(container)
+
+    setView(superContainer)
+
+    return this
+  }
+
+  private fun getStatusSummary(state: UnifiedPushSettingsState): String {
+    if (!state.enabled) {
+      return getString(R.string.UnifiedPushSettingsFragment__status_summary_disabled)
+    }
+    if (state.airGaped) {
+      return getString(R.string.UnifiedPushSettingsFragment__status_summary_air_gaped)
+    }
+    if (state.mollySocketUrl.isNullOrBlank()) {
+      return getString(R.string.UnifiedPushSettingsFragment__status_summary_mollysocket_url_missing)
+    }
+    return when (state.mollySocketOk) {
+      null -> getString(R.string.UnifiedPushSettingsFragment__status_summary_pending)
+      true -> getString(R.string.UnifiedPushSettingsFragment__ok)
+      false -> getString(R.string.UnifiedPushSettingsFragment__status_summary_mollysocket_server_not_found)
+    }
+  }
+
+  private fun getMollySocketUrlIcon(state: UnifiedPushSettingsState): DSLSettingsIcon? {
+    if (state.mollySocketUrl == null || state.mollySocketOk == null ) return null
+    return if (state.mollySocketOk) {
+      DSLSettingsIcon.from(R.drawable.ic_check_20)
+    } else {
+      DSLSettingsIcon.from(R.drawable.ic_alert)
+    }
+  }
+}

app/src/unifiedpush/java/im/molly/unifiedpush/components/settings/app/notifications/UnifiedPushSettingsViewModel.kt
@@ -1,0 +1,125 @@
+package im.molly.unifiedpush.components.settings.app.notifications
+
+import android.app.Application
+import android.content.pm.PackageManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.thoughtcrime.securesms.R
+import org.thoughtcrime.securesms.keyvalue.SignalStore
+import org.thoughtcrime.securesms.util.livedata.Store
+import org.unifiedpush.android.connector.UnifiedPush
+
+class UnifiedPushSettingsViewModel(private val application: Application) : ViewModel() {
+
+  private val store = Store(getState())
+
+  val state: LiveData<UnifiedPushSettingsState> = store.stateLiveData
+  var checkingServer = false
+
+  private fun getState(): UnifiedPushSettingsState {
+    val distributor = UnifiedPush.getDistributor(application)
+    var count = -1
+    var selected = -1
+
+    var distributors = UnifiedPush.getDistributors(application).map {
+      count++
+      if (it == distributor) {
+        selected = count
+      }
+      Distributor(
+        applicationId = it,
+        name = try {
+          val ai = application.packageManager.getApplicationInfo(it, 0)
+          /* When Android 13 will be supported:
+          val ai = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            application.packageManager.getApplicationInfo(it,
+              PackageManager.ApplicationInfoFlags.of(
+                PackageManager.GET_META_DATA.toLong()
+              )
+            )
+          } else {
+            application.packageManager.getApplicationInfo(it, 0)
+          }*/
+          application.packageManager.getApplicationLabel(ai)
+        } catch (e: PackageManager.NameNotFoundException) {
+          it
+        } as String)
+    }
+
+    if (distributors.isEmpty()) {
+      distributors = listOf(Distributor(
+        name = application.getString(R.string.UnifiedPushSettingsViewModel__no_distributor),
+        applicationId = "",
+      ))
+    }
+
+    return UnifiedPushSettingsState(
+      enabled = SignalStore.unifiedpush().enabled,
+      airGaped = SignalStore.unifiedpush().airGaped,
+      device = SignalStore.unifiedpush().device,
+      distributors = distributors,
+      selected = selected,
+      endpoint = SignalStore.unifiedpush().endpoint,
+      mollySocketUrl = SignalStore.unifiedpush().mollySocketUrl,
+      mollySocketOk = if (checkingServer) { null } else { SignalStore.unifiedpush().mollySocketOk },
+    )
+  }
+
+  fun setUnifiedPushEnabled(enabled: Boolean) {
+    SignalStore.unifiedpush().enabled = enabled
+    if (enabled) {
+      UnifiedPush.getDistributors(application).getOrNull(0)?.let {
+        UnifiedPush.saveDistributor(application, it)
+        UnifiedPush.registerApp(application)
+        // Do not enable if there is no distributor
+      } ?: return
+    } else {
+      UnifiedPush.unregisterApp(application)
+    }
+    SignalStore.unifiedpush().enabled = enabled
+    processNewStatus()
+    store.update { getState() }
+  }
+
+  fun setUnifiedPushAirGaped(airGaped: Boolean) {
+    SignalStore.unifiedpush().airGaped = airGaped
+    processNewStatus()
+  }
+
+  fun setUnifiedPushDistributor(distributor: String) {
+    UnifiedPush.saveDistributor(application, distributor)
+    UnifiedPush.registerApp(application)
+    store.update { getState() }
+  }
+
+  fun setMollySocketUrl(url: String?) {
+    SignalStore.unifiedpush().mollySocketUrl = if (url.isNullOrBlank()) {
+      null
+    } else if (url.last() != '/') {
+      "$url/"
+    } else {
+      url
+    }
+    processNewStatus()
+  }
+
+  private fun processNewStatus() {
+    if (with(SignalStore.unifiedpush()) {
+        this.enabled && !this.airGaped && this.endpoint.isNullOrBlank()
+      }) {
+      checkingServer = true
+      store.update { getState() }
+      //TODO
+      SignalStore.unifiedpush().mollySocketOk = true
+      checkingServer = false
+    }
+    store.update { getState() }
+  }
+
+  class Factory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+      return requireNotNull(modelClass.cast(UnifiedPushSettingsViewModel(application)))
+    }
+  }
+}